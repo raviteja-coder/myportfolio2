59363c31808dc0e4d6f523c4160aa8bc63c5660b
raviteja-coder <nakkaraviteja161616@gmail.com>
2024-09-10 11:21:19 +0530

    init

---
+++ src/components/Projects/index.js
@@ -0,0 +1,199 @@
+
+
+
+import React from 'react';
+import Header from '../Header';
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import './index.css';
+
+const Projects = () => {
+  return (
+    <div className='projects-container'>
+        <Header/>
+    <section id="projects" className="projects-section">
+       
+      <h2 className="projects-title">My Projects</h2>
+      <div className="project-cards">
+
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725376529/Screenshot_11_fcvoar.png" alt="Netflix Clone" />
+          <h1 className='project-title'>Netflix Clone</h1>
+          <p className='project-description'>This Netflix Clone project is a fully responsive web application that mimics the look and functionality of the popular streaming service. 
+            The project is done only html and css
+          </p>
+          <hr/>
+        
+          <div className='div'>
+            <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://ravinetflixapp.ccbp.tech' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725451315/Screenshot_12_fs0pta.png" alt="Netflix Clone" />
+          <h1 className='project-title'>Wekipedia Search Application</h1>
+          <p className='project-description'>
+            this project is consisnt of basic html and css. this is like mini google you want to search anything in this application.
+          </p>
+
+          <hr/>
+        
+          <div className='div'>
+            <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/bootstrap-BDzXOSnv.png" alt="bootstrap" className='technology-image-3'/>
+            <img src="	https://vanshdeep-verma.netlify.app/assets/js-removebg-preview-BZZkANDc.png" alt="javascript" className='technology-image-4'/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://ravitejawekiapp.ccbp.tech' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725514624/Screenshot_13_w48p0l.png" alt="Netflix Clone" />
+          <h1 className='project-title'>Food Munch App</h1>
+          <p className='project-description'>In this project I created a simple UI of any random restraunt which needs its stratup to have an impressive push on the customer by having a impressive UI based online app of its restraunt. So it can get digital customer and even get helpe in increasing its bussiness. It is a fronted UI of Restraunt Stratup providing multiple services.
+
+          </p>
+
+          <hr/>
+        
+          <div className='div'>
+            <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/bootstrap-BDzXOSnv.png" alt="bootstrap" className='technology-image-3'/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://ravitejafoodweb.ccbp.tech' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725612166/Screenshot_15_nof9ss.png" alt="Projects" />
+          <h1 className='project-title'>Project Files App</h1>
+          <p className='project-description'> The project files app consists of some projects done by me. you can visit my files and see my projects and contact me with your valuble feed back
+            .
+          </p>
+          <hr/>
+        
+          <div className='div'>
+          <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/bootstrap-BDzXOSnv.png" alt="bootstrap" className='technology-image-3'/>
+            <img src="	https://vanshdeep-verma.netlify.app/assets/js-removebg-preview-BZZkANDc.png" alt="javascript" className='technology-image-4'/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://rtprojectfiles.ccbp.tech/' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725612268/Screenshot_16_fzgci0.png" alt="Nxt Watch logo" />
+          <h1 className='project-title'>NXT Watch App</h1>
+          <p className='project-description'>
+            Nxt watch app is like a mini youtube you want to search whatever you want. see if content is related to you like and save the video.
+          </p>
+
+          <hr/>
+        
+          <div className='div'>
+            <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/bootstrap-BDzXOSnv.png" alt="bootstrap" className='technology-image-3'/>
+            <img src="	https://vanshdeep-verma.netlify.app/assets/js-removebg-preview-BZZkANDc.png" alt="javascript" className='technology-image-4'/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://rtnxtwatchapp.ccbp.tech/' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725612745/Screenshot_17_fhj9zk.png" alt="Prime video" />
+          <h1 className='project-title'>Amazon Prime Clone</h1>
+          <p className='project-description'>
+            In this Project I created a Amazon Prime App Clone. the app was contains videos and user friendly UI it makes good feels to user.
+          </p>
+
+          <hr/>
+        
+          <div className='div'>
+            <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/bootstrap-BDzXOSnv.png" alt="bootstrap" className='technology-image-3'/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/js-removebg-preview-BZZkANDc.png" alt="javascript" className='technology-image-4'/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://rtprimevideoapp.ccbp.tech/' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+        
+        
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725613973/Screenshot_18_tkpmwn.png" alt="scissor game" />
+          <h1 className='project-title'>Rock Paper Scissor Game</h1>
+          <p className='project-description'>
+            I am created a Game Rock Paper Scissor. first you see the rules and play the game. it is very funny game in this game three image are there in the UI you have to click to ant of them and see the result.
+          </p>
+
+          <hr/>
+        
+          <div className='div'>
+            <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/bootstrap-BDzXOSnv.png" alt="bootstrap" className='technology-image-3'/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/js-removebg-preview-BZZkANDc.png" alt="javascript" className='technology-image-4'/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://rtrpsgameapp.ccbp.tech/' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725637279/Screenshot_20_vngs58.png" alt="Gradient gen" />
+          <h1 className='project-title'>Gradient Generator</h1>
+          <p className='project-description'>
+            I am created a color Gradient Generator. this is the color you want fron top to bottom or left to right whatever you want. it apply the gradient in background.
+            </p>
+
+          <hr/>
+        
+          <div className='div'>
+            <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/bootstrap-BDzXOSnv.png" alt="bootstrap" className='technology-image-3'/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/js-removebg-preview-BZZkANDc.png" alt="javascript" className='technology-image-4'/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://rtgradientgen.ccbp.tech/' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+
+        <div className="project-card">
+        <img src="https://res.cloudinary.com/dayzpflw1/image/upload/v1725637105/Screenshot_19_trlgdy.png" alt="cowinash board" />
+          <h1 className='project-title'>Co-Win Dashboard</h1>
+          <p className='project-description'>
+            the co-win dashboars is a over view of total vaccinated people in covid time.
+               </p>
+
+          <hr/>
+        
+          <div className='div'>
+            <img src="https://vanshdeep-verma.netlify.app/assets/html-removebg-preview-Cm-BWZcT.png" alt="html" className="technology-image"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/css-removebg-preview-Mu73_D6d.png" alt="css" className="technology-image-2"/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/bootstrap-BDzXOSnv.png" alt="bootstrap" className='technology-image-3'/>
+            <img src="https://vanshdeep-verma.netlify.app/assets/js-removebg-preview-BZZkANDc.png" alt="javascript" className='technology-image-4'/>
+          </div>
+
+          <hr/>
+          <a className='a' href='https://rtcowindashbord.ccbp.tech/' target="_blank" rel="noopener noreferrer"> <p>Visit Project</p><FaArrowUpRightFromSquare className='icon'/></a>
+        </div>
+
+      </div>
+    </section>
+    </div>
+  );
+};
+
+export default Projects;
